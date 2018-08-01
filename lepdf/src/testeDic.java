cdb56c5e7f0c2e8a98764c6f629f73564b7c1e9f
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-08-01 20:52:29 -0300

    Alteração da classe do teste de dicionário

--- lepdf/src/testeDic.java
+++ lepdf/src/testeDic.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-class Spelling {
+class testeDic {
   public static void main(String[] args) throws IOException {
     ResourceBundle ptC = ResourceBundle.getBundle("resources.palavrasAcentuadas", new Locale("pt", "BR"));
     System.out.println(ptC.getString("braco"));
