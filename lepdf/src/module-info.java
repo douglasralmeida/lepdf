47dc41367910d936a48e8f76c6438c2724a57c9d
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-02-22 00:49:59 -0300

    Reinformação de dependências

--- lepdf/src/module-info.java
+++ lepdf/src/module-info.java
@@ -1,12 +1,9 @@
 module lePdf {
 	requires java.base;
-	requires jdk.jdwp.agent;
-	requires itextpdf.kernel;
-	requires itextpdf.io;
-	requires itextpdf.layout;
+	requires kernel;
+	requires io;
+	requires layout;
 	requires com.sun.jna;
-	
-	requires transitive org.slf4j;
 
 	exports lePdf;
 }
