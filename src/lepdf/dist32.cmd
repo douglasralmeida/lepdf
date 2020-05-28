eb9a6776dd83fa1344aaef00b6a9ba33032bbce1
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2020-05-28 12:11:32 -0300

    Rollback para Java 12 por problemas de desempenho

--- src/lepdf/dist32.cmd
+++ src/lepdf/dist32.cmd
@@ -1,8 +1,8 @@
 SETLOCAL
 
-SET "TOOLCHAINDIR=C:\dev\java-14.x86\bin"
+SET "TOOLCHAINDIR=C:\dev\java-12.x86\bin"
 SET PATH=%TOOLCHAINDIR%
 
-jlink --add-modules lePdf --module-path lib;bin --launcher gui=lePdf/lePdf.lePdf --output ../../bin/jre32 --strip-debug --strip-java-debug-attributes --compress=0 --no-header-files --no-man-pages --ignore-signing-information
+jlink --add-modules lePdf --module-path lib;bin --launcher gui=lePdf/lePdf.lePdf --output ../../bin/jre32 --strip-debug --compress=0 --no-header-files --no-man-pages --ignore-signing-information
 
 ENDLOCAL
